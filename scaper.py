2e71b8dbf002f3b29bdd6cd402e121051b301aa2
mgordon25 <mgordon25@gmail.com>
2019-11-02 14:52:40 -0700

    added search settings.  added url output

--- scaper.py
+++ scaper.py
@@ -1,6 +1,11 @@
 from craigslist import CraigslistHousing
+import search_settings as ss
+
 from pprint import pprint
-cl = CraigslistHousing(site='sfbay', area='sfc', category='apa', filters={'max_price': 3500, 'min_price': 2000})
+cl = CraigslistHousing(site='sfbay', area=ss.CRAIGSLIST_SUB, category='apa', filters={'max_price': 3500, 'min_price': 2000})
 results = cl.get_results(sort_by='newest', geotagged=True, limit=20)
 for result in results:
-    pprint(result)
+    #pprint(result, depth=1, width=100)
+    search_results = open('search_results.txt', 'a+')     
+    print(result['url'], file=search_results)
+    search_results.close()
--- search_settings.py
+++ search_settings.py
@@ -16,5 +16,18 @@ CRAIGSLIST_SITE = 'sfbay'
 # this case I want to search the peninsula subdirectory
 CRAIGSLIST_SUB = 'pen'
 
-
+# A list of neighborhoods and coordinates that you want to look for apartments in.  Any listing that has coordinates
+# attached will be checked to see which area it is in.  If there's a match, it will be annotated with the area
+# name.  If no match, the neighborhood field, which is a string, will be checked to see if it matches
+# anything in NEIGHBORHOODS.
+BOXES = {
+    "redwood_city": [
+        [37.409339, -122.335912],
+        [37.562461,	-122.140858],
+    ],
+    "san_mateo": [
+        [37.409339, -122.446209],
+        [37.65479, -122.23692],
+    ]
+}
 
