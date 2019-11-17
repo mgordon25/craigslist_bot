43e581bd0a7c8ee1a8707800f621f590ab3c4c9a
mgordon25 <mgordon25@gmail.com>
2019-11-17 11:37:17 -0800

    converted results to list and removed writing to text file

--- scaper.py
+++ scaper.py
@@ -5,36 +5,37 @@ import os
 import string
 import smtplib
 from email.message import EmailMessage
+#import test_email
+
+
+# -- TODO --
+#  convert results to list    !!!!!!!!!done
+#  no need for text file
+#  make email a class and pass list to it.
+#  insert list into html and send
+
 
 if os.path.exists('search_results.txt'):
     os.remove('search_results.txt')
 cl = CraigslistHousing(site='sfbay', area=ss.CRAIGSLIST_SUB, category='apa', filters={'max_price': 3000, 'min_price': 2000})
+
+results = []
 results = cl.get_results(sort_by='newest', geotagged=True, limit=20)
-href = "<a href="
-href2 = "\>"
-href3 = "</a>"
+
+print(type(results))
+lineList = list()
+    
+#for result in results:
+#    search_results = open('search_results.txt', 'a+')
+#    search_results.write(result["url"] + "\n")
+    
+#lineList = [line.rstrip('\n') for line in open('search_results.txt')]
+
 for result in results:
-  
-    #pprint(href + result["url"] + href2 + "holder" + '</a>', depth=1, width=100)
-    #print(result['url'])
+    lineList.append(result["url"])
     
-    search_results = open('search_results.txt', 'a+')
-    search_results.write(result["url"] + "\n")
-    print(result["url"])
+print(type(lineList))
+
+for line in lineList:
     
-    #print(href + result["url"] + href2 + "holder" + href3, file = search_results)
-    
-    #search_results = str(search_results)
-    #print(type(search_results))
-    #print(search_results, href3, file = search_results)
-    #search_results = search_results.rstrip('/')
-    #search_results.close()
-# for result in results:
-    #pprint(result['url'], depth=1, width=100)
-    #search_results = open('search_results.html', 'a+')     
-    #print('<a href="{result['url']}"file=search_results)
-    #print(result['url'], file=search_results)
-    #search_results.close()
-with open('search_results.txt', 'r') as f2:
-    data = f2.read()
-    print(data)
+    print(line)
