e9f8aa67beb3fc5fdce0cbedf81685469b8d8571
mgordon25 <mgordon25@gmail.com>
2019-11-17 13:48:50 -0800

    created function to email list of links

--- scaper.py
+++ scaper.py
@@ -6,13 +6,85 @@ import string
 import smtplib
 from email.message import EmailMessage
 #import test_email
+import smtplib
+import getpass
+from email.message import EmailMessage
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def emailFunction(CLList):
+    print(CLList)
+    print("You have entered function.")
+
+    sender_email = 'mgdevtest@gmail.com'
+    receiver_email = 'mgordon25@gmail.com'
+    gmail_password = ''
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "multipart test"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+
+    print(receiver_email)
+
+    email_text = """\
+    From: 
+    To: 
+    Subject: 
 
 
-# -- TODO --
-#  convert results to list    !!!!!!!!!done
-#  no need for text file
-#  make email a class and pass list to it.
-#  insert list into html and send
+    """ #%(sent_from, ", ".join(to), subject, body)
+
+    #html = """\
+
+            #<a href="https://sfbay.craigslist.org/pen/apa/d/belmont-unbeatable-value-for-top-floor/7021410954.html">Real Python</a> 
+
+        # """
+
+    email_text2 = str(CLList)
+    # -- TODO: Insert text into hyperlink
+
+    # convert to plain/html MIMEText objects
+    part1 = MIMEText(email_text, "plain")
+    #part2 = MIMEText(html, "html")
+    part3 = MIMEText(email_text2, "plain")
+
+    # Add HTML/plain text to MIMEMultipart message
+    # the email client will try to render the last part first
+
+    message.attach(part1)
+    #message.attach(part2)
+    message.attach(part3)
+
+
+
+    #try:
+    #    gmail_password = input("Password: ")
+    
+    #except:
+    #    print("error")
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(sender_email, gmail_password)
+        server.sendmail(sender_email, receiver_email , message.as_string())
+        server.close()
+    
+
+        print('Email sent')
+
+    except:
+        print('Somthing went wrong...')
+
+
+
+    # -- TODO --
+#  convert results to list      !!!!!!!!!!!!!!!!!!!!!!!!!done
+#  no need for text file        !!!!!!!!!!!!!!!!!!!!!!!!!done
+#  make email a function and pass list to it.      !!!!!!done
+#  insert list into html and send                  !!!!!!done
+#  format links
 
 
 if os.path.exists('search_results.txt'):
@@ -25,8 +97,7 @@ results = cl.get_results(sort_by='newest', geotagged=True, limit=20)
 print(type(results))
 lineList = list()
     
-#for result in results:
-#    search_results = open('search_results.txt', 'a+')
+#for result in result#    search_results = open('search_results.txt', 'a+')
 #    search_results.write(result["url"] + "\n")
     
 #lineList = [line.rstrip('\n') for line in open('search_results.txt')]
@@ -39,3 +110,6 @@ print(type(lineList))
 for line in lineList:
     
     print(line)
+
+
+emailFunction(lineList)
